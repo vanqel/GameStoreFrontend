6463c234336b7d93d5dc5db042801a9671e3e90f
vanqel <vanqel@dtechs.io>
2024-03-31 20:29:25 +0300

    remove fail

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import NotFound from './pages/not_found/not-found'
 import Market from './pages/market/market'
 import Auth from './widget/login/login'
 import Item from './pages/itenpage/item'
-import Fail from "./features/payment/ui/fail";
 import Success from "./features/payment/ui/success";
 import Profile from "./widget/profile/profile";
 import Register from "./widget/register/register";
@@ -27,7 +26,6 @@ const App = () => {
         <Route component={Home} exact path="/" />
         <Route component={Market} path="/market" />
         <Route component={Item} path="/product" />
-        <Route component={Fail} path="/fail"/>
         <Route component={Profile} path="/profile"/>
         <Route component={Success} path="/success"/>
         <Route component={NotFound} />
