b8c43a1c4aed3e1494ea735cc11bb2b1a6d91b0e
vanqel <vanqel@dtechs.io>
2024-03-31 17:52:23 +0300

    test

--- src/features/payment/api/succes.js
+++ src/features/payment/api/succes.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 async function sendSuccessPayment(OutSum: string, InvId: number, SignatureValue: string): Promise<void> {
     try {
+        console.log(OutSum, InvId, SignatureValue)
         const response = await axios.post('https://openstorebackendkotlin.onrender.com/kassa/success', {
             OutSum,
             InvId,
